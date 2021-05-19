7ed21232b991f2a95759203484286e280ed4be99
Brannon Wadforth <whiskey@digitalnode.io>
2021-05-19 21:58:18 +1000

    Adding comment regarding bug that needs to be fixed

--- src/common/interface.c
+++ src/common/interface.c
@@ -32,6 +32,7 @@ void get_interface(char err_buf[], pcap_if_t *pcap_iface) {
     }
 
     *pcap_iface = *tmp;
+    // TODO this is a bug, we're freeing interfaces, tmp points to interface and pcap_iface attemps to point to tmp.
     pcap_freealldevs(interfaces);
 };
 
